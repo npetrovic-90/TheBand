177c25f34d4310838fa5c6a3f8d7354967b715d0
Nemanja Petrovic <nemanja90petrovic@hotmail.com>
2021-10-16 17:14:27 +0200

    Validating using custom attribute.

--- TheBand/BandAPI/Models/AlbumForCreatingDto.cs
+++ TheBand/BandAPI/Models/AlbumForCreatingDto.cs
@@ -1,14 +1,26 @@
-﻿using System;
+﻿using BandAPI.ValidationAttributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BandAPI.Models
 {
-	public class AlbumForCreatingDto
+	[TitleAndDescriptionAttribute]
+	public class AlbumForCreatingDto //: IValidatableObject
 	{
+		[Required]
 		public string Title { get; set; }
+		[MaxLength(400)]
 		public string Description { get; set; }
-		
+
+		//public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		//{
+		//	if(Title == Description)
+		//	{
+		//		yield return new ValidationResult("Title and Description need to be different!", new[] { "AlbumForCreatingDto" });
+		//	}
+		//}
 	}
 }
